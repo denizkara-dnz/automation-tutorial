df03f40b0aec9dd705c36b03dbf2cb9c1fc59d9b
DESKTOP-P92JSTH\VAIO <denizkara_dnz@hotmail.com>
2023-01-12 23:22:32 +0300

    ikinci ders

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -4,11 +4,15 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
+import java.util.List;
+
 public class BaseTest {
 
     WebDriver driver = new ChromeDriver();
 
     @Test
+    /*
+    //ilk ders
     void titleControl() {
         driver.get("https://www.google.com/");
         //driver.get("https://www.haberturk.com/");
@@ -20,5 +24,25 @@ public class BaseTest {
         elem.sendKeys("deneme");
         driver.quit();
     }
-
+    */
+    void getList() {
+        driver.get("https://www.milliyet.com.tr/");
+        //driver.findElement(By.linkText("Gündem")).click();
+        WebElement element = driver.findElement(By.className("main-menu__mobile-scroll"));
+        List<WebElement> headerMenuBtn = element.findElements(By.tagName("li"));
+//        for (int i = 0; i < headerMenuBtn.size(); i++) {
+//            WebElement menuText = headerMenuBtn.get(i);
+//            if (menuText.getText().equals("Gündem")) {
+//                menuText.click();
+//                break;
+//            }
+//        }
+        for (WebElement lst:headerMenuBtn) {
+            if (lst.getText().equals("Gündem")){
+                lst.click();
+                break;
+            }
+        }
+        driver.quit();
+    }
 }
